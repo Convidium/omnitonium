e1f998f639e83dc6257096a64e39377e76ccd02b
Convidium <denyssianchuk0@gmail.com>
2024-12-19 18:05:31 +0200

    removed useless comment

--- src/components/functions/setGlobalCSSValues.js
+++ src/components/functions/setGlobalCSSValues.js
@@ -1,9 +1,6 @@
 function setCssVariable(colors) {
     const contrastColor = colors.mostContrastingColor;
     const root = document.documentElement;
-    // colors.forEach((color, index) => {
-    //     root.style.setProperty(`--coverColor${index + 1}`, `rgb(${color.r}, ${color.g}, ${color.b})`);
-    // });
     root.style.setProperty(`--contrastingColor`, `rgb(${contrastColor.r}, ${contrastColor.g}, ${contrastColor.b})`);
 }
 
